f733321c1902b82e56179d2aa273bbeae6ab4498
Yaimee <37310514+Yaimee@users.noreply.github.com>
2022-11-15 16:51:46 +0100

    rasmus push

--- src/main/java/com/example/portfolio/PortfolioApplication.java
+++ src/main/java/com/example/portfolio/PortfolioApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PortfolioApplication {
-
+    int i;
     public static void main(String[] args) {
         SpringApplication.run(PortfolioApplication.class, args);
     }
